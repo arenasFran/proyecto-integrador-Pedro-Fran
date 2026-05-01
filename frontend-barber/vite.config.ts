d80b3f9e2606544fef86d041fc4fb2c5ecba6369
MacDuki <pcmyact@hotmail.com>
2026-05-01 15:59:31 -0300

    feat: configurar proxy en Vite para conectar con backend

--- frontend-barber/vite.config.ts
+++ frontend-barber/vite.config.ts
@@ -4,5 +4,12 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-    
+  server: {
+    proxy: {
+      '/auth': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
+  }
 })

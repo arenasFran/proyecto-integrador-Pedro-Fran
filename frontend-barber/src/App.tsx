963584337b2de6f6706ed46c6bd792ba11a778d5
MacDuki <pcmyact@hotmail.com>
2026-05-01 17:08:05 -0300

    feat: configurar rutas en App.tsx

--- frontend-barber/src/App.tsx
+++ frontend-barber/src/App.tsx
@@ -1,21 +1,29 @@
-import { motion } from "framer-motion";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { RegisterPage } from './pages/public/RegisterPage';
+import { RecoveryPage } from './pages/public/RecoveryPage';
+import LoginPage from './pages/public/LoginPage';
 
 function App() {
   return (
-    <div className="min-h-screen bg-zinc-950 flex items-center justify-center ">
-      <motion.div 
-        initial={{ opacity: 0, scale: 0.5 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 0.5 }}
-        className="p-8 bg-zinc-900 border border-zinc-800 rounded-2xl shadow-2xl text-center"
-      >
-        <h1 className="text-3xl font-bold text-white mb-2">
-          Frontend <span className="text-sky-800">Configurado</span>
-        </h1>
-        <p className="text-zinc-400">Vite + React + TS + Tailwind + Framer</p>
-      </motion.div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPageWrapper />} />
+        <Route path="/recovery" element={<RecoveryPage />} />
+        <Route path="/" element={<RegisterPageWrapper />} />
+      </Routes>
+    </Router>
   );
+}
+
+function RegisterPageWrapper() {
+  const navigate = useNavigate();
+  
+  const handleNavigateToLogin = () => {
+    navigate('/login');
+  };
+
+  return <RegisterPage onNavigateToLogin={handleNavigateToLogin} />;
 }
 
 export default App;

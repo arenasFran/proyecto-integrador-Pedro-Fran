cd106e079a4057f0c11900104ca6328356b781bc
MacDuki <pcmyact@hotmail.com>
2026-05-13 23:18:48 -0300

    refactor: actualizar auth.2fa.controller con nuevos imports
    - Cambiar import desde auth.utils (findUserByEmail, validatePassword)
    - Sin cambios en lógica de 2FA

--- backend-barber/src/modules/auth/controllers/auth.2fa.controller.ts
+++ backend-barber/src/modules/auth/controllers/auth.2fa.controller.ts
@@ -1,8 +1,8 @@
-import { Request, Response } from "express";
 import crypto from "crypto";
+import { Request, Response } from "express";
 import jwt, { SignOptions } from "jsonwebtoken";
-import { findUserByEmail, validatePassword } from "../services/users.services";
 import mailer from "../../../config/mailer";
+import { findUserByEmail, validatePassword } from "../utils/auth.utils";
 
 const jwtExpiresIn = (process.env.JWT_EXPIRES_IN || "1h") as SignOptions["expiresIn"];
 

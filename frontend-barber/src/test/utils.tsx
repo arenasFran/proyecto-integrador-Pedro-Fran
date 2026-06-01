a9505a0e4dca2da289744816a433010ec2c3d85d
MacDuki <pcmyact@hotmail.com>
2026-06-01 12:58:20 -0300

    fix(utils): corregir tipo de preloadedState en test utils
    - Elimina import no usado de PreloadedState
    - Cambia el tipo de preloadedState a Partial<RootState> para
      permitir estados parciales al crear el store de test
    - Agrega type assertion necesaria para satisfacer configureStore

--- frontend-barber/src/test/utils.tsx
+++ frontend-barber/src/test/utils.tsx
@@ -1,19 +1,19 @@
 import React, { type PropsWithChildren } from 'react';
 import { Provider } from 'react-redux';
-import { configureStore, type PreloadedState } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { render, type RenderOptions } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import authReducer from '../store/slices/authSlice';
 import type { RootState } from '../store';
 
 interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
-  preloadedState?: PreloadedState<RootState>;
+  preloadedState?: Partial<RootState>;
 }
 
-function createTestStore(preloadedState?: PreloadedState<RootState>) {
+function createTestStore(preloadedState?: Partial<RootState>) {
   return configureStore({
     reducer: { auth: authReducer },
-    preloadedState,
+    preloadedState: preloadedState as RootState | undefined,
   });
 }
 

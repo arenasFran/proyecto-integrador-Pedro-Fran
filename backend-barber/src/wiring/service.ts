a2ebe51a25d7d7df8ac7217cbbac0629e324ff72
MacDuki <pcmyact@hotmail.com>
2026-06-18 16:22:58 -0300

    Fase 5a: Inline GetAllServicesUseCase en ServiceController

--- backend-barber/src/wiring/service.ts
+++ backend-barber/src/wiring/service.ts
@@ -1,12 +1,10 @@
-import { GetAllServicesUseCase } from '../application/use-cases/service/GetAllServicesUseCase';
 import { StaticServiceRepository } from '../infrastructure/repositories/static/StaticServiceRepository';
 import { ServiceController } from '../interface-adapters/controllers/service/ServiceController';
 import { createServiceRouter } from '../interface-adapters/routes/service.routes';
 
 export const buildServiceRouter = () => {
   const repo = new StaticServiceRepository();
-  const getAll = new GetAllServicesUseCase(repo);
-  const controller = new ServiceController(getAll);
+  const controller = new ServiceController(repo);
 
   return createServiceRouter({ serviceController: controller });
 };
